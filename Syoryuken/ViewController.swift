5c3da53f7c99e51e2cce8b3fc07b9aa54d2ff901
imai0708 <asano120pnpn@gmail.com>
2022-10-21 13:59:37 +0900

    Syoryuken

--- Syoryuken/ViewController.swift
+++ Syoryuken/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var Ryu: UIImageView!  //リュウの画像
+    var imageArrayAttack : Array<UIImage> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let image = UIImage(named: "attak1")
+        Ryu.image = image
+       
+        
+        while let attackImage = UIImage(named: "attak\(imageArrayAttack.count+1)") {
+            imageArrayAttack.append(attackImage)
+        }
+      
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func activation(_ sender: Any) {
+        Ryu.animationImages = imageArrayAttack
+        Ryu.animationDuration = 2
+        Ryu.animationRepeatCount = 1
+        Ryu.startAnimating()
+        
+    }
+    
 }
 
+    
